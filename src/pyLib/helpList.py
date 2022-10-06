4627f31809158ebd00abd3e8702f3132731134a9
Pedro Cazelatto <PedroCazelatto@users.noreply.github.com>
2022-10-06 15:14:35 -0300

    Detailed help list

---
+++ src/pyLib/helpList.py
@@ -0,0 +1,21 @@
+from rich.align import Align
+from rich.style import Style
+from rich.panel import Panel
+from rich.console import RenderableType
+from rich.tree import Tree
+
+from textual.widget import Widget
+
+from pyLib.infoLists import *
+
+class helpList(Widget):
+    
+    helpBar = Tree("Comandos")
+    
+    for idx, seq in enumerate(validCommands):
+        helpBar.add(seq).add(helpContents[idx])
+    
+    def render(self) -> RenderableType:
+        return Panel(self.helpBar,
+                     title= "Comandos",
+                     border_style= Style(color= "bright_magenta"))
