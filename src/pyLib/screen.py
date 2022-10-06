3875e55e343366cc0c9d6a6184f777e5e0138c35
Pedro Cazelatto <PedroCazelatto@users.noreply.github.com>
2022-10-06 15:24:57 -0300

    Help bar and CmdLine size fix

--- src/pyLib/screen.py
+++ src/pyLib/screen.py
@@ -13,13 +13,14 @@ from pyLib.interface import interface
 
 class screen(App):
     show_help = Reactive(False)
+    helpBarSize = 50
     
     async def on_load(self):
         await self.bind("ctrl+a", "toggle_help", "Ajuda  ")
         await self.bind("ctrl+c", "quit", "Saída  ")
     
     def watch_show_help(self, show_help: bool):
-        self.barra.animate("layout_offset_x", 0 if show_help else 40)
+        self.helpBar.animate("layout_offset_x", 0 if show_help else self.helpBarSize)
         
     def action_toggle_help(self):
         self.show_help = not self.show_help
@@ -27,20 +28,20 @@ class screen(App):
     # O que acontece ao rodar o programa (SETUP)
     async def on_mount(self):
         
-        header = Header(tall=False) # Cria o cabecalho
-        await self.view.dock(header) # Adiciona o cabecalho no topo
+        header = Header(tall=False)
+        await self.view.dock(header)
                 
         footer = Footer()
-        await self.view.dock(footer, edge="bottom") # Adiciona o rodape
+        await self.view.dock(footer, edge="bottom")
 
-        self.barra = helpList() # Cria uma barra
-        await self.view.dock(self.barra, edge="right", size=40, z=1)
+        self.helpBar = helpList()
+        await self.view.dock(self.helpBar, edge= "right", size= self.helpBarSize, z= 1)
         
-        self.barra.layout_offset_x = 40
+        self.helpBar.layout_offset_x = self.helpBarSize
 
         homeGrid = await self.view.dock_grid()
         
-        homeGrid.add_row("row1", fraction= 3)
-        homeGrid.add_row("row2", fraction= 1, max_size= 13, min_size= 3)
+        homeGrid.add_row("row1")
+        homeGrid.add_row("row2", size= 12)
         homeGrid.add_column("col")
         homeGrid.place(interface(), cmdLine())
