b03e3e12293eaee5bf6331d9606f5ef6ef1e290d
Pedro Cazelatto <PedroCazelatto@users.noreply.github.com>
2022-10-07 22:10:06 -0300

    File removal implemented

---
+++ src/pyLib/virtualDisc.py
@@ -0,0 +1,41 @@
+import os
+
+from rich.align import Align
+from rich.style import Style
+from rich.panel import Panel
+from rich.console import RenderableType
+from rich.tree import Tree
+
+from textual.reactive import Reactive
+from textual.widget import Widget
+
+import os
+
+class _virtualDisc(Widget):
+    _instance = None
+    
+    # def createFile(self, fileName: str):
+    #     if os.path.exists("./root/" + fileName):
+    #         return False
+    #     with open("./root/" + fileName):
+    #         pass
+    
+    def deleteFile(self, fileName: str) -> bool:
+        if os.path.exists("./root/" + fileName):
+            os.remove("./root/" + fileName)
+            return True
+        return False
+
+    def render(self) -> RenderableType:
+        archives = Tree("root")
+        for arch in os.scandir("./root"):
+            if arch.is_file():
+                archives.add(arch.name)
+        return Panel(archives,
+                     title="Disco (Pasta externa)",
+                     border_style= Style(color= "bright_cyan"))
+
+def virtualDisc():
+    if _virtualDisc._instance is None:
+        _virtualDisc._instance = _virtualDisc()
+    return _virtualDisc._instance
